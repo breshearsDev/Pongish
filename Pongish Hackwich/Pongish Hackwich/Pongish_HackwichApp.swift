031ac5c004c9d5264708df35e01fe11418e8c497
breshearsDev <144831261+breshearsDev@users.noreply.github.com>
2024-05-13 08:49:49 -0600

    Add files via upload
    Files

---
+++ Pongish Hackwich/Pongish Hackwich/Pongish_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pongish_HackwichApp.swift
+//  Pongish Hackwich
+//
+//  Created by Breshears, Rob - TMH on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Pongish_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
